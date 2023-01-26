42e0d98a67e5095016fcac867bcbf67ddf8bf258
Timofey Kirillov <timofey.kirillov@flant.com>
2023-01-26 17:09:37 +0300

    chore: fixes

--- .toolbox/record-werf-build.sh
+++ .toolbox/record-werf-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-asciinema rec --title "werf build" --command "echo '$ werf build --repo k3d-registry.sample-app.test:5000/sample-app' ; werf build --repo k3d-registry.sample-app.test:5000/sample-app"
+asciinema rec --title "werf build" --command ".toolbox/type-and-execute.sh werf build --repo k3d-registry.sample-app.test:5000/sample-app"
